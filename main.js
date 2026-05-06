9c0391034de021c3f1006f57513b8c097a73ed56
kaden-emrich <125313480+kaden-emrich@users.noreply.github.com>
2026-05-06 12:31:52 -0400

    added background ambient mode

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Kaden Emrich
-// SohCahToa
+
 const canvas = document.getElementById("main-canvas");
 const ctx = canvas.getContext("2d");
 
@@ -28,7 +28,9 @@ const displayF2 = document.getElementById("display-f2");
 const sliderF3 = document.getElementById("slider-f3");
 const displayF3 = document.getElementById("display-f3");
 
-const size = 500;
+const overrideArea = document.getElementById("override-area");
+
+var size = 1000;
 
 var autoDraw = true;
 
@@ -42,10 +44,12 @@ function updateSize() {
     backgroundCanvas.width = size;
     backgroundCanvas.height = size;
 
-    document.getElementById("canvas-area").style.width = `${size}px`;
-    document.getElementById("canvas-area").style.height = `${size}px`;
+    ctx.lineWidth = size / 200;
 
-    document.getElementById("progress-area").style.width = `${size}px`;
+    // document.getElementById("canvas-area").style.width = `${size}px`;
+    // document.getElementById("canvas-area").style.height = `${size}px`;
+
+    // document.getElementById("progress-area").style.width = `${size}px`;
 }
 updateSize();
 
@@ -53,6 +57,9 @@ var mods = [
     [100, 10],
     [40, -9]
 ];
+
+
+
 
 /*
 
@@ -142,11 +149,11 @@ function lcmArr(arr) {
     return last;
 }
 
-function updateVars() {
+function updateVars(values,rb) {
     // console.log('updating vars...'); // for debugging
     replay = replayCheckbox.checked;
     autoDraw = autoCheckbox.checked;
-    doRainbow = rainbowCheckbox.checked;
+    doRainbow = rainbowCheckbox.checked || rb;
 
     boost = parseInt(sliderBoost.value);
 
@@ -156,15 +163,29 @@ function updateVars() {
 
     mods = [];
 
-    if(sliderF1.value != 0) {
-        mods.push([size * 0.25, parseInt(sliderF1.value)]);
+    if(values) {
+        overrideArea.innerText = "Values overridden with: " + values.toString();
+        for(let i = 0; i < values.length; i++) {
+            if(values[i] == 0) {
+                continue;
+            }
+            let sizeModifier = (0.5 ** (i+2));
+            mods.push([size * sizeModifier, values[i]]);
+        }
     }
-    if(sliderF2.value != 0) {
-        mods.push([size * 0.125, parseInt(sliderF2.value)]);
-    }
-    if(sliderF3.value != 0) {
-        mods.push([size * 0.0625, parseInt(sliderF3.value)]);
-    }
+    else {
+        overrideArea.innerText = "";
+
+        if(sliderF1.value != 0) {
+            mods.push([size * 0.25, parseInt(sliderF1.value)]);
+        }
+        if(sliderF2.value != 0) {
+            mods.push([size * 0.125, parseInt(sliderF2.value)]);
+        }
+        if(sliderF3.value != 0) {
+            mods.push([size * 0.0625, parseInt(sliderF3.value)]);
+        }
+    }    
 
     if(autoDraw) {
         clearCanvas();
@@ -222,10 +243,42 @@ function clearCanvas() {
     progressBar.style.width = `0%`;
 }
 
+var normalizeAnimationTime = false;
+var maxAnimationTime = 10000; // ms
+var frameTime = 1000/30;
 var drawInterval;
-function draw() {
+function draw(callback) {
+    frameTime = 1000/30;
+
+    // console.time("animation calculation");
     calculate();
+    // console.timeEnd("animation calculation");
+
     var index = 0;
+
+    // if(points.length * frameTime > maxAnimationTime) {
+    //     frameTime = 60000 / points.length;
+    //     console.log(`Animation too long, Frame time updated to: ${frameTime}ms`); // for debugging
+    // }
+
+    if(normalizeAnimationTime) {
+        boost = 1;
+        frameTime = maxAnimationTime / points.length;
+
+        while(frameTime < 20) {
+            boost++;
+            frameTime = maxAnimationTime / (points.length / boost);
+        }
+
+        while(frameTime * points.length / boost < maxAnimationTime) {
+            frameTime ++;
+        }
+
+        var calulatedAnimationTime = (points.length / boost) * frameTime;
+
+        // console.log(`----------------------------------\nNormalizing animation time to ${maxAnimationTime}ms, \nFrame time updated to: ${frameTime}ms, \nDraws per frame: ${boost}, \nCalulated animation time: ${calulatedAnimationTime}ms`); // for debugging
+        // console.log(`frame count: ${points.length}`); // for debugging
+    }
     // ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     clearInterval(drawInterval);
@@ -233,7 +286,7 @@ function draw() {
     var displayedProgress = 0;
     progressBar.style.width = `${displayedProgress}%`;
 
-    drawInterval = setInterval(() => {
+    let drawFunc = () => {
         // ctx.clearRect(0, 0, canvas.width, canvas.height);
         
         for(let i = index; i < index + boost && i < points.length; i++) {
@@ -279,22 +332,35 @@ function draw() {
         }
 
         var progress = Math.floor(100 * index / points.length);
+        progress = progress > 100 ? 100 : progress;
+        // console.log(progress); // for debugging
         if(progress > displayedProgress) {
             displayedProgress = progress;
             progressBar.style.width = `${displayedProgress}%`;
         }
 
-        if(index >= points.length) {
-            stickCtx.clearRect(0, 0, stickCanvas.width, stickCanvas.height);
+        if(index >= points.length && drawInterval) {
             clearInterval(drawInterval);
+            drawInterval = undefined;
+            stickCtx.clearRect(0, 0, stickCanvas.width, stickCanvas.height);
+
+            // console.timeEnd("animation-time"); // for debugging
+            
+            if(callback) callback();
 
             if(replay) draw();
         }
-    }, 1000/60);
+    }
+
+    // console.time("animation-time"); // for debugging;
+    drawInterval = setInterval(() => {
+        drawFunc();
+    }, frameTime);
 }
 
 function quickDraw() {
     calculate();
+    
     var index = 0;
     // ctx.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -332,6 +398,70 @@ function printMods() {
     console.log((JSON.stringify(mods)));
 }
 
+var buryTransparency = 0.75;
+function bury() {
+    ctx.fillStyle = `rgba(0,0,0,${buryTransparency})`;
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
+
+let animatedBuryFrameRate = 1000/30;
+function animatedBury(time = 1000, trans = 0.75) {
+    let frameCount = time / animatedBuryFrameRate;
+    let a = 1 - (1-trans) ** (1 / frameCount);
+
+    // console.log(a); // for debugging
+
+    for(let i = 0.0; i < time; i += animatedBuryFrameRate) {
+        setTimeout(() => {
+            ctx.fillStyle = `rgba(0,0,0,${a})`;
+            ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        }, i);
+    }
+
+    // setTimeout(() => {
+    //     console.log("animated bury done");
+    // }, time); // for debugging
+}
+
+let maxRandomTime = 20;
+function drawRandom(callback, count = 3, rb = true) {
+    stop();
+    let times = [];
+
+    for(let i = 0; i < count; i++) {
+        let nextTime = Math.floor(Math.random() * (maxRandomTime * 2 + 1) - maxRandomTime);
+        times.push(nextTime);
+    }
+
+    updateVars(times, rb);
+    // console.log(mods.toString()); // for debugging
+    draw(callback);
+}
+
+var autoplayDelay = 1000;
+var continueAutoplay = true;
+function startAutoplay() {
+    continueAutoplay = true;
+    normalizeAnimationTime = true;
+    autoplay();
+}
+function autoplay() {
+    let buryTime = 1000;
+
+    if(!continueAutoplay) return;
+
+    drawRandom(() => { 
+        setTimeout(() => {
+            animatedBury(buryTime);
+            setTimeout(() => {
+                autoplay();
+            }, buryTime + 1000);
+        }, autoplayDelay); 
+    });
+    
+}
+
+
 function init() {
     updateVars();
 
@@ -340,6 +470,10 @@ function init() {
 
         backgroundCtx.drawImage(canvas, 0, 0);
     }, 1000 / 60);
+
+    if(backgroundMode) {
+        startAutoplay();
+    };
 }
 
 init();
